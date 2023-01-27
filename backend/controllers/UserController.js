7254a54b6cdfb1983bc174b24ab60a33975a2612
Cristiano Muroni <muronislick@hotmail.com>
2023-01-26 22:26:25 -0300

    função de check user

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -8,33 +8,33 @@ module.exports = class UserControler {
 
         if(!name) {
             res.status(422).json({message: 'O nome é obrigatório'})
-            return
-        }
+            return;
+        };
         if(!email) {
             res.status(422).json({message: 'O email é obrigatório'})
-            return
-        }
+            return;
+        };
         if(!phone) {
             res.status(422).json({message: 'O telefone é obrigatório'})
-            return
-        }
+            return;
+        };
         if(!password) {
             res.status(422).json({message: 'A senha é obrigatória'})
-            return
-        }
+            return;
+        };
         if(!confirmpassword) {
             res.status(422).json({message: 'A confirmação de senha é obrigatória'})
-            return
-        }
+            return;
+        };
         if(confirmpassword !== password) {
             res.status(422).json({message: 'a senha deve ser igual a confirmação de senha'});
-            return
-        }
+            return;
+        };
         const userExists = await User.findOne({email: email});
         if(userExists) {
             res.status(422).json({message: 'Por favor , utilize outro email'})
-            return
-        }
+            return;
+        };
 
         const salt = await bcrypt.genSalt(12);
         const passwordHash = await bcrypt.hash(password, salt);
@@ -50,7 +50,7 @@ module.exports = class UserControler {
             await createUserToken(newUser, req, res);
         } catch (error) {
             res.status(500).json({message: error});
-        }
+        };
     };
 
     static async login(req, res) {
@@ -58,12 +58,12 @@ module.exports = class UserControler {
 
         if(!email) {
             res.status(422).json({message: 'O email é obrigatório'})
-            return
-        }
+            return;
+        };
         if(!password) {
             res.status(422).json({message: 'A senha é obrigatória'})
-            return
-        }
+            return;
+        };
 
         const user = await User.findOne({email: email});
         if(!user) {
@@ -81,6 +81,17 @@ module.exports = class UserControler {
             return;
         };
         await createUserToken(user, req, res);
-        
+        return;        
+    };
+
+    static async checkUser(req, res) {
+        let currentUser
+        console.log(req.headers.authorization);
+        if(req.headers.authorization) {
+
+        }else {
+            currentUser = null;
+        }; 
+        res.status(200).send(currentUser);       
     };
 };
--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const UserControler = require('../controllers/UserController');
+const User = require('../models/User');
 
 router.post('/register', UserControler.register);
 router.post('/login', UserControler.login);
+router.get('/checkuser', UserControler.checkUser);
 
 module.exports = router;
