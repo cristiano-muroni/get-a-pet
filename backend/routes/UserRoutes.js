c90933074d8a5e6c9c9b7c4e2c0519e4957b23ef
Cristiano Muroni <muronislick@hotmail.com>
2023-02-02 18:01:43 -0300

    Criando midleware de upload de imagens e add a rota de aupdate

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
 const UserControler = require('../controllers/UserController');
 const verifyToken = require('../helpers/verify-token');
+const { imageUpload } = require('../helpers/image-upload');
 
 router.post('/register', UserControler.register);
 router.post('/login', UserControler.login);
 router.get('/checkuser', UserControler.checkUser);
 router.get('/:id', UserControler.getUserById);
-router.patch('/edit/:id', verifyToken, UserControler.editUser);
+router.patch('/edit/:id', verifyToken, imageUpload.single("image"), UserControler.editUser);
 
 module.exports = router;
