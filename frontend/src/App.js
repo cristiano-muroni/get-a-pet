6c5d5fce0f4e65b5df7e8f8deaf759ac4efa3f13
Cristiano Muroni <muronislick@hotmail.com>
2023-02-15 21:47:22 -0300

    refactore

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,16 +12,16 @@ import Home from "../src/components/pages/Home";
 function App() {
   return (
     <Router>
-      <Switch>        
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/register">
-            <Register />
-            </Route>
-            <Route path="/">
-            <Home />
-            </Route>        
+      <Switch>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
       </Switch>
     </Router>
   );
