bdfa4512556605321e1027c3edcf704e4eae01e4
Cristiano Muroni <muronislick@hotmail.com>
2023-02-15 21:44:36 -0300

    insert react-router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,66 +1,30 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React from "react";
 
 /* components */
-import Navbar from './components/layout/Navbar'
-import Footer from './components/layout/Footer'
-import Message from './components/layout/Message'
-import Container from './components/layout/Container'
 
 /* pages */
-import Home from './components/pages/Home'
-import Login from './components/pages/Auth/Login'
-import Register from './components/pages/Auth/Register'
-import Profile from './components/pages/User/Profile'
-import AddPet from './components/pages/Pet/AddPet'
-import MyPets from './components/pages/Pet/MyPets'
-import EditPet from './components/pages/Pet/EditPet'
-import PetDetails from './components/pages/Pet/PetDetails'
-import MyAdoptions from './components/pages/Pet/MyAdoptions'
+import Login from "../src/components/pages/Auth/Login";
+import Register from "../src/components/pages/Auth/Register";
+import Home from "../src/components/pages/Home";
 
 /* contexts */
-import { UserProvider } from './context/UserContext'
 function App() {
   return (
     <Router>
-      <UserProvider>
-        <Navbar />
-        <Message />
-        <Container>
-          <Switch>
-            <Route path="/login">
-              <Login />
-            </Route>
-            <Route path="/register">
-              <Register />
-            </Route>
-            <Route path="/user/profile">
-              <Profile />
-            </Route>
-            <Route path="/pet/add">
-              <AddPet />
-            </Route>
-            <Route path="/pet/edit/:id">
-              <EditPet />
-            </Route>
-            <Route path="/pet/mypets">
-              <MyPets />
-            </Route>
-            <Route path="/pet/myadoptions">
-              <MyAdoptions />
-            </Route>
-            <Route path="/pet/:id">
-              <PetDetails />
+      <Switch>        
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/register">
+            <Register />
             </Route>
             <Route path="/">
-              <Home />
-            </Route>
-          </Switch>
-        </Container>
-        <Footer />
-      </UserProvider>
+            <Home />
+            </Route>        
+      </Switch>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
