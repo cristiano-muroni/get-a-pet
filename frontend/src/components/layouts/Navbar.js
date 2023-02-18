4646a47612e30bbdbee1c286c26fc2c9c6a48e91
Cristiano Muroni <muronislick@hotmail.com>
2023-02-17 21:42:29 -0300

    Criando CSS module do Navbar

--- frontend/src/components/layouts/Navbar.js
+++ frontend/src/components/layouts/Navbar.js
@@ -1,9 +1,10 @@
 import {Link} from 'react-router-dom';
 import Logo from '../../assets/img/logo.png';
+import styles from './Navbar.module.css';
 function Navbar() {
     return (
-        <nav>
-            <div>
+        <nav className={styles.navbar}>
+            <div className={styles.navbar_logo}>
               <img src={Logo} alt='Get A Pet'/>
             </div>
             <ul>
