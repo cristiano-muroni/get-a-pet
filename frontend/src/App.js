775675a1be437676c022d79e083892960c27a55b
Cristiano Muroni <muronislick@hotmail.com>
2023-02-21 22:29:59 -0300

    Criando context

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,39 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React from "react";
 
 /* components */
-import Navbar from './components/layouts/Navbar';
-import Footer from './components/layouts/Footer';
-import Container from './components/layouts/Container';
+import Navbar from "./components/layouts/Navbar";
+import Footer from "./components/layouts/Footer";
+import Container from "./components/layouts/Container";
 
 /* pages */
-import Login from '../src/components/pages/Auth/Login';
-import Register from '../src/components/pages/Auth/Register';
-import Home from '../src/components/pages/Home';
+import Login from "../src/components/pages/Auth/Login";
+import Register from "../src/components/pages/Auth/Register";
+import Home from "../src/components/pages/Home";
 
 /* contexts */
+import { UserProvider } from "./context/UserContext";
+
 function App() {
   return (
     <Router>
-      <Navbar />
-      <Container>
-        <Switch>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/register">
-            <Register />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </Container>
-      <Footer />
+      <UserProvider>
+        <Navbar />
+        <Container>
+          <Switch>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/register">
+              <Register />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </Container>
+        <Footer />
+      </UserProvider>
     </Router>
   );
 }
